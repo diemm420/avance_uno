2deb8381831ba16904f4d83aed6c85450d4704fd
diemm420 <david_maciasm@outlook.com>
2016-08-25 19:10:06 -0500

    Add files via upload

---
+++ ProyectosAndroid - copia/app/build/generated/source/buildConfig/debug/softdaniv/com/san32_movil/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package softdaniv.com.san32_movil;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "softdaniv.com.san32_movil";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
